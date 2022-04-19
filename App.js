248360ce803c5f8f07a529b08d044dadfc7fea96
weslley.hub <weslleyfernando716092gmail.com>
2022-04-19 20:57:52 -0300

    questao01

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
-export default function App() {
-  return (
+export default function Click(){
+  
+  const [contar, setContador] = useState(0);
+  function incrementar(){
+    setContador(contar+1);
+  } return ( 
+   
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <View style={styles.caixa}>
+      <Text>Você clicou {contar} vezes</Text>
+      <Button title='CLICK' onPress={incrementar}/>
+      </View>
+  </View>
+
+ );
 }
 
 const styles = StyleSheet.create({
+  
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    height:'100%',
+    backgroundColor: '#a9a9a9',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 1,
   },
+
+  caixa: {
+    height:'15%',
+    width:'80%',
+    alignItems: 'center',
+    backgroundColor:'#008080',
+    justifyContent: 'center',
+    fontSize: 15,
+  }
 });
